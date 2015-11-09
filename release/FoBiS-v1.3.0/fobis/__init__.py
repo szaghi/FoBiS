c1c545366e14d5c9b57775dc9ea1257d7466b087
Stefano Zaghi <stefano.zaghi@gmail.com>
2015-11-09 12:38:48 +0100

    sanitize release directory

--- release/FoBiS-v1.3.0/fobis/__init__.py
+++
@@ -1,1 +0,0 @@
-"""FoBiS.py main package"""
--- release/FoBiS-v1.5.0/scripts/FoBiS.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-"""Wrapper of FoBiS.py program extracted from fobis package"""
-from fobis.fobis import main
-if __name__ == '__main__':
-  main()
